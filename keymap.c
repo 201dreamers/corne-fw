4bfe3dff0b74c6e132138b241045fd6b166743f4
Dmytro Hakman <dmytro.hakman@sleepnumber.com>
2023-04-02 16:59:18 +0300

    change keymap and rgb effects

--- keymap.c
+++ keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,        KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,         KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,   KC_BSPACE,
        KC_LCTRL,        KC_A,        KC_S,        KC_D,        KC_F,        KC_G,         KC_H,        KC_J,        KC_K,        KC_L,   KC_SCOLON,    KC_QUOTE,
         KC_LALT,        KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,         KC_N,        KC_M,    KC_COMMA,      KC_DOT,    KC_SLASH,    KC_ENTER,
-                                               KC_LGUI,   KC_LSHIFT,     FN_MO23,      FN_MO13,    KC_SPACE,   KC_ESCAPE
+                                               KC_LGUI,     FN_MO23,   KC_LSHIFT,     KC_SPACE,     FN_MO13,   KC_ESCAPE
     ),
 
     [_LWR] = LAYOUT_split_3x6_3(
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RSE] = LAYOUT_split_3x6_3(
-         KC_TAB,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,     KC_TRNS,
-        KC_TRNS,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,     KC_TRNS,
-        KC_TRNS,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,     KC_TRNS,
+          KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,        KC_F7,       KC_F8,       KC_F9,      KC_F10,      KC_F11,      KC_F12,
+        KC_TRNS,     KC_BRID,     KC_BRIU,     KC_VOLD,     KC_VOLU,     KC_MUTE,      RGB_MOD,     RGB_VAI,     RGB_SAI,     RGB_HUI,     RGB_TOG,     KC_TRNS,
+        KC_TRNS,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,     RGB_RMOD,     RGB_VAD,     RGB_SAD,     RGB_HUD,       RESET,     KC_TRNS,
                                                KC_TRNS,     KC_TRNS,     KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS
     ),
 
     [_ADJ] = LAYOUT_split_3x6_3(
-          RESET,     RGB_HUI,     RGB_SAI,     RGB_MOD,     RGB_VAI,     RGB_TOG,      KC_BTN3,     KC_BTN1,     KC_BTN2,     KC_BTN4,       20480,     KC_TRNS,
-        KC_TRNS,     RGB_HUD,     RGB_SAD,    RGB_RMOD,     RGB_VAD,       KC_NO,      KC_MS_L,     KC_MS_D,     KC_MS_U,     KC_MS_R,       20484,     KC_TRNS,
-        KC_TRNS,     KC_VOLD,     KC_VOLU,     KC_MUTE,     KC_BRID,     KC_BRIU,      KC_WH_L,     KC_WH_D,     KC_WH_U,     KC_WH_R,       KC_NO,     KC_TRNS,
-                                               KC_TRNS,     KC_TRNS,     KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS
+          KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,
+          KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,
+          KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,        KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,
+                                                 KC_NO,       KC_NO,     KC_TRNS,      KC_TRNS,       KC_NO,       KC_NO
     )
 };
 
